9ba15c1188785598f9f4759581f6961a6f55674b
KZW <k.a.zwierzynski@gmail.com>
2018-01-18 11:46:54 +0100

    goodParts- up to closures, new file for variousTutorials

--- JavaScript/goodParts/program.js
+++ JavaScript/goodParts/program.js
@@ -268,6 +268,77 @@ var myObject = function() {
             return value;
         }
     };      //We are not assigning a function to myObject. We are assigning the result of invoking
-}();        //that function. Notice the () on the last line.
+}();        //that function. Notice the () on the last line. The function returns an object containing
+            //two methods
+
+
+// Create a maker function called quo. It makes an object with a get_status method and a private
+// status property.
+var quo = function (status) {
+    return {
+        get_status: function ( ) {
+            return status;
+        }
+    };
+};
+// Make an instance of quo.
+var myQuo = quo("amazed");
+document.writeln(myQuo.get_status( ));
+
+//  fade the background
+// Define a function that sets a DOM node's color
+// to yellow and then fades it to white.
+var fade = function (node) {
+    var level = 1;
+    var step = function ( ) {
+        var hex = level.toString(16);
+        node.style.backgroundColor = '#FFFF' + hex + hex;
+        if (level < 15) {
+            level += 1;
+            setTimeout(step, 100);
+        }
+    };
+
+    setTimeout(step, 100);
+};
+    fade(document.body);
+
+// BAD EXAMPLE
+// Make a function that assigns event handler functions to an array of nodes the wrong way.
+// When you click on a node, an alert box is supposed to display the ordinal of the node.
+// But it always displays the number of nodes instead.
+var add_the_handlers = function (nodes) {
+    var i;
+    for (i = 0; i < nodes.length; i += 1) {
+        nodes[i].onclick = function (e) {
+            alert(i);
+        };
+    }
+};
+// END BAD EXAMPLE
+
+// BETTER EXAMPLE
+// Make a function that assigns event handler functions to an array of nodes the right way.
+// When you click on a node, an alert box will display the ordinal of the node.
+var add_the_handlers = function (nodes) {
+    var i;
+    for (i = 0; i < nodes.length; i += 1) {
+        nodes[i].onclick = function (i) {
+            return function (e) {
+                alert(e);
+            };
+        }(i);
+    }
+};
+
+add_the_handlers(Document);
+
+
+
+
+
+
+
+
 
 
