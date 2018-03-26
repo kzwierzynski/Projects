4d7e541afdf5c3cd12838ede1b6a026671597136
KZW <k.a.zwierzynski@gmail.com>
2018-03-26 10:35:11 +0200

    Grid3 added, restructured folders

--- JavaScript/Bookmarker/js/main.js
+++ JavaScript/Bookmarker/js/main.js
@@ -73,6 +73,7 @@ function deleteBookmark(url){
 function fetchBookmarks(){
   // Get bookmarks from localStorage
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  console.log(bookmarks)
   // Get output id
   var bookmarksResults = document.getElementById('bookmarksResults');
 
